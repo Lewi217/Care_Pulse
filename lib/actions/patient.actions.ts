7c6d083b9ed77eb38159aad12398ad081a3344fd
wanjohi lewis <wanjohilewi7@gmail.com>
2024-08-12 23:55:26 +0300

    init

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,25 @@
+import { ID, Query } from "node-appwrite"
+import { users } from "../appwrite.config"
+
+export const createUser = async (user: CreateUserParams) => {
+    try{
+        const newuser = await users.create(
+            ID.unique(),
+            user.email,
+            user.phone,
+            undefined,
+            user.name
+        )
+    } catch (error: any) {
+        // Check existing user
+        if (error && error?.code === 409) {
+          const documents = await users.list([
+            Query.equal("email", [user.email])
+          ])
+
+          return documents?.users[0]
+
+        }
+        
+    }   
+}
